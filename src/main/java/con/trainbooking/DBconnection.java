8c3152647f571c37f957884e2762488991667ab3
Rithanya26 <rithanyasubbu26@gmail.com>
2026-01-09 14:29:51 +0530

    Create DBconnection.java

---
+++ src/main/java/con/trainbooking/DBconnection.java
@@ -0,0 +1,15 @@
+package con.trainbooking;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/train_booking";
+    private static final String USER = "root"; // your MySQL username
+    private static final String PASSWORD = "password"; // your MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
